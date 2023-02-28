8b88b0ed26076267155afb4a2698e87edb4432f9
Paul Giberson <pgiberson@higherlogic.com>
2023-02-27 17:18:18 -0700

    refactor: extract github interaction

--- dist/index.js
+++ dist/index.js
@@ -108,6 +108,107 @@ exports.formatFiles = formatFiles;
 
 /***/ }),
 
+/***/ 5928:
+/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
+
+"use strict";
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.getFileChanges = void 0;
+const core = __importStar(__nccwpck_require__(2186));
+const github_1 = __nccwpck_require__(5438);
+function getFileChanges(token) {
+    var _a, _b, _c, _d;
+    return __awaiter(this, void 0, void 0, function* () {
+        // Debug log the payload.
+        core.debug(`Payload keys: ${Object.keys(github_1.context.payload)}`);
+        // Get event name.
+        const eventName = github_1.context.eventName;
+        // Define the base and head commits to be extracted from the payload.
+        let base;
+        let head;
+        switch (eventName) {
+            case 'pull_request':
+                base = (_b = (_a = github_1.context.payload.pull_request) === null || _a === void 0 ? void 0 : _a.base) === null || _b === void 0 ? void 0 : _b.sha;
+                head = (_d = (_c = github_1.context.payload.pull_request) === null || _c === void 0 ? void 0 : _c.head) === null || _d === void 0 ? void 0 : _d.sha;
+                break;
+            case 'push':
+                base = github_1.context.payload.before;
+                head = github_1.context.payload.after;
+                break;
+            default:
+                throw new Error(`This action only supports pull requests and pushes, ${github_1.context.eventName} events are not supported. ` +
+                    "Please submit an issue on this action's GitHub repo if you believe this in correct.");
+        }
+        // Log the base and head commits
+        core.info(`Base commit: ${base}`);
+        core.info(`Head commit: ${head}`);
+        // Ensure that the base and head properties are set on the payload.
+        if (!base || !head) {
+            throw new Error(`The base and head commits are missing from the payload for this ${github_1.context.eventName} event. ` +
+                "Please submit an issue on this action's GitHub repo.");
+        }
+        // Use GitHub's compare two commits API.
+        // https://developer.github.com/v3/repos/commits/#compare-two-commits
+        const response = yield (0, github_1.getOctokit)(token).rest.repos.compareCommitsWithBasehead({
+            basehead: `${base}...${head}`,
+            owner: github_1.context.repo.owner,
+            repo: github_1.context.repo.repo
+        });
+        // Ensure that the request was successful.
+        if (response.status !== 200) {
+            throw new Error(`The GitHub API for comparing the base and head commits for this ${github_1.context.eventName} event returned ${response.status}, expected 200. ` +
+                "Please submit an issue on this action's GitHub repo.");
+        }
+        // Ensure that the head commit is ahead of the base commit.
+        if (response.data.status !== 'ahead') {
+            throw new Error(`The head commit for this ${github_1.context.eventName} event is not ahead of the base commit. ` +
+                "Please submit an issue on this action's GitHub repo.");
+        }
+        if (response.data.files === undefined) {
+            throw new Error('Unexpected response from GitHub API, files property is undefined.');
+        }
+        // Get the changed files from the response payload.
+        return response.data.files;
+    });
+}
+exports.getFileChanges = getFileChanges;
+
+
+/***/ }),
+
 /***/ 8657:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
@@ -205,69 +306,16 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 const core = __importStar(__nccwpck_require__(2186));
-const github = __importStar(__nccwpck_require__(5438));
+const github_1 = __nccwpck_require__(5928);
 const format_1 = __nccwpck_require__(6610);
 const input_1 = __nccwpck_require__(8657);
 function run() {
-    var _a, _b, _c, _d;
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            const context = github.context;
             // resolve inputs
             const inputs = yield (0, input_1.getInputs)();
-            // Debug log the payload.
-            core.debug(`Payload keys: ${Object.keys(context.payload)}`);
-            // Get event name.
-            const eventName = context.eventName;
-            // Define the base and head commits to be extracted from the payload.
-            let base;
-            let head;
-            switch (eventName) {
-                case 'pull_request':
-                    base = (_b = (_a = context.payload.pull_request) === null || _a === void 0 ? void 0 : _a.base) === null || _b === void 0 ? void 0 : _b.sha;
-                    head = (_d = (_c = context.payload.pull_request) === null || _c === void 0 ? void 0 : _c.head) === null || _d === void 0 ? void 0 : _d.sha;
-                    break;
-                case 'push':
-                    base = context.payload.before;
-                    head = context.payload.after;
-                    break;
-                default:
-                    throw new Error(`This action only supports pull requests and pushes, ${context.eventName} events are not supported. ` +
-                        "Please submit an issue on this action's GitHub repo if you believe this in correct.");
-            }
-            // Log the base and head commits
-            core.info(`Base commit: ${base}`);
-            core.info(`Head commit: ${head}`);
-            // Ensure that the base and head properties are set on the payload.
-            if (!base || !head) {
-                throw new Error(`The base and head commits are missing from the payload for this ${context.eventName} event. ` +
-                    "Please submit an issue on this action's GitHub repo.");
-            }
-            // Use GitHub's compare two commits API.
-            // https://developer.github.com/v3/repos/commits/#compare-two-commits
-            const response = yield github
-                .getOctokit(inputs.token)
-                .rest.repos.compareCommitsWithBasehead({
-                basehead: `${base}...${head}`,
-                owner: context.repo.owner,
-                repo: context.repo.repo
-            });
-            // Ensure that the request was successful.
-            if (response.status !== 200) {
-                throw new Error(`The GitHub API for comparing the base and head commits for this ${context.eventName} event returned ${response.status}, expected 200. ` +
-                    "Please submit an issue on this action's GitHub repo.");
-            }
-            // Ensure that the head commit is ahead of the base commit.
-            if (response.data.status !== 'ahead') {
-                throw new Error(`The head commit for this ${context.eventName} event is not ahead of the base commit. ` +
-                    "Please submit an issue on this action's GitHub repo.");
-            }
-            // Get the changed files from the response payload.
-            const files = response.data.files;
-            //check if files is undefined
-            if (files === undefined) {
-                throw new Error('Error pulling files from response payload.');
-            }
+            // compare commits
+            const files = yield (0, github_1.getFileChanges)(inputs.token);
             // Format the changed files.
             const { allFormatted, addedFormatted, modifiedFormatted, removedFormatted, renamedFormatted, addedModifiedFormatted } = yield (0, format_1.formatFiles)(files, inputs.format);
             // Log the output values.
